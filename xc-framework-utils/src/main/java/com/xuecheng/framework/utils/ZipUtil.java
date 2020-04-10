d97a9fa3a60b9c4d947d97507186e31746ad5671
15619980227 <1913475244@qq.com>
2020-04-10 17:12:35 +0800

    ZIPUtil.java静态代码检测已完成

--- xc-framework-utils/src/main/java/com/xuecheng/framework/utils/ZipUtil.java
+++ xc-framework-utils/src/main/java/com/xuecheng/framework/utils/ZipUtil.java
@@ -1,38 +1,43 @@
 package com.xuecheng.framework.utils;
 
-
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 
+/**
+ * ZIP工具
+ *
+ * @author yuanYuan
+ * @version 1.0
+ * @date
+ * @deprecated 解压工具
+ */
 public class ZipUtil {
 
     /**
      * 解压zip文件
-     * @param zipFilePath
-     * @param targetPath
-     * @throws ZipException
+     *
+     * @param zipFilePath ZIP文件路径
+     * @param targetPath  解压后目标文件路径
+     * @throws ZipException 解压异常
      */
-    public static void unzip(String zipFilePath,String targetPath) throws Exception{
+    public static void unzip(String zipFilePath, String targetPath) throws Exception {
         ZipFile zipFile = new ZipFile(zipFilePath);
         zipFile.extractAll(targetPath);
     }
 
     /**
      * 解压zip文件（带密码）
-     * @param zipFilePath
-     * @param targetPath
-     * @param password
-     * @throws ZipException
+     *
+     * @param zipFilePath ZIP文件路径
+     * @param targetPath  解压后目标文件路径
+     * @param password    解压密码
+     * @throws ZipException 解压异常
      */
-    public static void unzip(String zipFilePath,String password,String targetPath) throws Exception{
+    public static void unzip(String zipFilePath, String password, String targetPath) throws Exception {
         ZipFile zipFile = new ZipFile(zipFilePath);
         if (zipFile.isEncrypted()) {
             zipFile.setPassword(password);
         }
         zipFile.extractAll(targetPath);
-    }
-
-    public static void main(String[] args) throws Exception {
-        ZipUtil.unzip("F:\\develop\\upload\\upload.zip","F:\\develop\\upload\\zip\\");
     }
 }
